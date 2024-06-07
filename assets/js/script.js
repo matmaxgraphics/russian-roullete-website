97c365509b387c92e56d2174d3a6d1678e58205a
matmaxgraphics <gbadamosimateen@gmail.com>
2024-06-07 23:06:40 +0100

    added other sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,11 @@ function classToggle() {
 const navbarLink = document.querySelector(".navbar__Link-toggle");
 navbarLink.addEventListener("click", classToggle);
 
-
-
 function copyToClipboard() {
-  const walletAddress = document.querySelector('.volx-address');
+  const walletAddress = document.querySelector(".bullet-address");
 
   if (walletAddress) {
-    const textarea = document.createElement('textarea');
+    const textarea = document.createElement("textarea");
     textarea.value = walletAddress.textContent;
     document.body.appendChild(textarea);
     textarea.select();
@@ -22,8 +20,31 @@ function copyToClipboard() {
 
     document.body.removeChild(textarea);
 
-    alert('Copied to clipboard: ' + walletAddress.textContent);
+
+    var snackBar = document.getElementById("snackbar");
+    snackBar.className = "show";
+    setTimeout(function () {
+      snackBar.className = snackBar.className.replace("show", "");
+    }, 3000);
   } else {
-    console.error('Span element not found.');
+    console.error("Span element not found.");
   }
 }
+
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
+    let content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      // console.log(content.style.maxHeight);
+    }
+  };
+});
